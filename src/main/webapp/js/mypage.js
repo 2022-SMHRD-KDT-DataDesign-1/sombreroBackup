4b78e570826f4999f577a5fd8301499659e21c38
cyzlcyzl <cyzlcyzl09@gmail.com>
2023-04-03 14:36:17 +0900

    230403/염혜정/1:1문의답변확인끝

--- src/main/webapp/js/mypage.js
+++ src/main/webapp/js/mypage.js
@@ -3,7 +3,7 @@ console.log("js 파일 연결")
 let user_status = document.getElementById("hidden").innerText;
 console.log(user_status)
 
-
+/* 상태 나타내기 */
 
 if(user_status == "입금완료"){
 	document.getElementById("deposit").style.backgroundColor = "black";
@@ -26,3 +26,36 @@ if(user_status == "입금완료"){
 	document.getElementById("v_span").style.color = "white"
 	document.getElementById("v_span").innerHTML = "<em class='em_status'>1</em><br>후기 남기기"
 }
+
+
+/* 1:1 관련 */
+let hiddenClass = document.querySelectorAll('.i_hidden');
+
+console.log(hiddenClass.length);
+
+/* 답변 및 내용 가려놓기 */
+for(let i = 0; i<hiddenClass.length; i++){
+	hiddenClass[i].style.display = 'none';
+}
+
+/* 클릭하면 답변 보이기 함수 */
+let showAnswer = (i_cnt) => {
+	console.log('ck이벤트')
+	console.log(i_cnt);
+	
+	let hidden1Td = document.querySelector('#i_1hidden'+i_cnt);
+	let hidden2Td = document.querySelector('#i_2hidden'+i_cnt);
+	
+	if(hidden1Td.style.display=='' && hidden2Td.style.display==''){
+		hidden1Td.style.display='none'
+		hidden2Td.style.display='none'
+	}else if(hidden1Td.style.display=='none' && hidden2Td.style.display=='none'){
+		hidden1Td.style.display=''
+		hidden2Td.style.display=''	
+	}
+}
+
+
+
+
+
